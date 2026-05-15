c2f06db074245069a72f982bdd7881dc61beeb3e
Stanislav Palikyrzha <stanislavpalikyrzha@gmail.com>
2026-05-15 13:34:00 +0300

    add login service

---
+++ lib/Auth.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+final class Auth
+{
+    public static function attemptLogin(string $email, string $password): ?array
+    {
+        $user = UserRepository::findByEmail($email);
+        if ($user === null) {
+            return null;
+        }
+
+        if (!password_verify($password, $user['password_hash'])) {
+            return null;
+        }
+
+        session_regenerate_id(true);
+        $_SESSION['user_id'] = (int) $user['id'];
