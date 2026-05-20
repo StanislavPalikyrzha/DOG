4ef09ccdc173bb85fd69fe871c8535e870bc8c99
Vladimir Vinokurov <17vinokurov@gmail.com>
2026-05-20 11:12:00 +0300

    support conditional template sections

--- lib/TemplateEngine.php
+++ lib/TemplateEngine.php
@@ -16,3 +16,21 @@ final class TemplateEngine
                 $key = $matches[1];
                 $content = $matches[2];
                 return !empty($safe[$key]) ? $content : '';
+            },
+            $templateHtml
+        ) ?? $templateHtml;
+
+        $templateHtml = preg_replace_callback(
+            '/{{(uppercase|lowercase):([a-zA-Z0-9_]+)}}/',
+            static function (array $matches) use ($safe): string {
+                $value = $safe[$matches[2]] ?? '';
+                return $matches[1] === 'uppercase' ? strtoupper($value) : strtolower($value);
+            },
+            $templateHtml
+        ) ?? $templateHtml;
+
+        $templateHtml = str_replace('{{today}}', date('d.m.Y'), $templateHtml);
+        $templateHtml = str_replace('{{now}}', date('d.m.Y H:i'), $templateHtml);
+
+        return preg_replace_callback(
+            '/{{([a-zA-Z0-9_]+)}}/',
