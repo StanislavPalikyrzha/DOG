57a083fd93fc2daed5b4f0bd6317a6b42a6108a8
Vladimir Vinokurov <17vinokurov@gmail.com>
2026-05-21 16:06:00 +0300

    write pdf page content stream

--- lib/PdfBuilder.php
+++ lib/PdfBuilder.php
@@ -15,3 +15,20 @@ final class PdfBuilder
                 continue;
             }
             foreach (self::wrap($line, 90) as $chunk) {
+                $wrapped[] = $chunk;
+            }
+        }
+
+        $content = "BT\n/F1 12 Tf\n50 790 Td\n16 TL\n";
+        $content .= '(' . self::escapeText($title) . ") Tj\nT*\n";
+        foreach ($wrapped as $line) {
+            $content .= '(' . self::escapeText($line) . ") Tj\nT*\n";
+        }
+        $content .= "ET";
+
+        $objects = [];
+        $objects[] = '1 0 obj << /Type /Catalog /Pages 2 0 R >> endobj';
+        $objects[] = '2 0 obj << /Type /Pages /Count 1 /Kids [3 0 R] >> endobj';
+        $objects[] = '3 0 obj << /Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >> endobj';
+        $objects[] = '4 0 obj << /Length ' . strlen($content) . " >> stream\n" . $content . "\nendstream endobj";
+        $objects[] = '5 0 obj << /Type /Font /Subtype /Type1 /BaseFont /Helvetica >> endobj';
